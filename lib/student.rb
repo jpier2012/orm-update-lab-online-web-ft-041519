c2e2b42b864cdf332fb5e93a188ab3ba6f97677a
James Pier <jpier2012@gmail.com>
2019-05-17 17:09:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,5 +12,14 @@ class Student
     @id = id
   end
 
+  def create_table
+    sql = <<-SQL
+      CREATE TABLE students IF NOT EXISTS (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER        
+      )
+    SQL
+  end
 
 end
